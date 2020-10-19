96fc8770c7c1006087de6670769486b9b6078871
TrixCal <traytonlarson01@gmail.com>
2020-10-19 10:50:19 -0500

    Modified Program to add and display movies

--- Program.cs
+++ Program.cs
@@ -10,44 +10,62 @@ namespace MediaLibrary
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
-
             logger.Info("Program started");
-
-            // Movie movie = new Movie
-            // {
-            //     mediaId = 123,
-            //     title = "Greatest Movie Ever, The (2020)",
-            //     director = "Jeff Grissom",
-            //     // timespan (hours, minutes, seconds)
-            //     runningTime = new TimeSpan(2, 21, 23),
-            //     genres = { "Comedy", "Romance" }
-            // };
-
-            // Console.WriteLine(movie.Display());
-
-            // Album album = new Album
-            // {
-            //     mediaId = 321,
-            //     title = "Greatest Album Ever, The (2020)",
-            //     artist = "Jeff's Awesome Band",
-            //     recordLabel = "Universal Music Group",
-            //     genres = { "Rock" }
-            // };
-            // Console.WriteLine(album.Display());
-
-            // Book book = new Book
-            // {
-            //     mediaId = 111,
-            //     title = "Super Cool Book",
-            //     author = "Jeff Grissom",
-            //     pageCount = 101,
-            //     publisher = "",
-            //     genres = { "Suspense", "Mystery" }
-            // };
-            // Console.WriteLine(book.Display());
-
             string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
             logger.Info(scrubbedFile);
+
+            MovieFile movieFile = new MovieFile(scrubbedFile);
+
+            string choice = "";
+            do
+            {
+                //Choices
+                Console.WriteLine("1) Add Movie");
+                Console.WriteLine("2) Display Movies");
+                Console.WriteLine("Enter to quit");
+                //input
+                choice = Console.ReadLine();
+                logger.Info($"User choice: {choice}");
+
+                if(choice == "1")
+                {
+                    Movie movie = new Movie();
+                    //request and input title
+                    Console.WriteLine("Enter movie title: ");
+                    movie.title = Console.ReadLine();
+                    //verify title
+                    if(movieFile.isUniqueTitle(movie.title))
+                    {
+                        //input genres
+                        string input;
+                        do
+                        {
+                            //request and input genre
+                            Console.WriteLine("Enter genre (or 'done' to quit)");
+                            input = Console.ReadLine();
+                            if(input != "done" && input.Length > 0)
+                            {
+                                movie.genres.Add(input);
+                            }
+                        } while(input != "done");
+                        if(movie.genres.Count == 0)
+                        {
+                            movie.genres.Add("(no genres listed)");
+                        }
+                        //add movie
+                        movieFile.AddMovie(movie);
+                    }
+                }
+                else if(choice == "2")
+                {
+                    foreach(Movie m in movieFile.Movies)
+                    {
+                        Console.WriteLine(m.Display());
+                    }
+                }
+                Console.ReadKey();
+                Console.Clear();
+            } while(choice == "1" && choice == "2");
 
             logger.Info("Program ended");
         }
