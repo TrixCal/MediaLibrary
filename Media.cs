7a56e6e6168e9e24461b6b974b7ed56abeddf115
TrixCal <traytonlarson01@gmail.com>
2022-02-28 14:06:24 -0600

    Added Book class

--- Media.cs
+++ Media.cs
@@ -34,4 +34,15 @@ namespace MediaLibrary{
             return $"Id: {mediaId}\nTitle: {title}\nArtist: {artist}\nLabel: {recordLabel}\nGenres: {string.Join(", ", genres)}\n";
         }
     }
+    public class Book : Media
+    {
+        public string author { get; set; }
+        public UInt16 pageCount { get; set; }
+        public string publisher { get; set; }
+
+        public override string Display()
+        {
+            return $"Id: {mediaId}\nTitle: {title}\nAuthor: {author}\nPages: {pageCount}\nPublisher: {publisher}\nGenres: {string.Join(", ", genres)}\n";
+        }
+    }
 }
