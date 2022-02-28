28410938288c629c558f8ed6d69ca933621dc5b8
TrixCal <traytonlarson01@gmail.com>
2022-02-28 12:57:35 -0600

    Initial Commit +Nlog

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using NLob.Web;
+
+namespace MediaLibrary
+{
+    class Program
+    {
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            logger.Info("Program Started");
+
+            Console.WriteLine("Hello World!");
+
+            logger.Info("Program Ended");
+        }
+    }
+}
